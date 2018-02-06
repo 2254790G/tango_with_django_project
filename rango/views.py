4ad4714df4ca39457777ec8d09a6d28b22e8231b
Amy Gillies <2254790g@student.gla.ac.uk>
2018-02-06 12:52:26 +0000

    Chapter 8

--- rango/views.py
+++ rango/views.py
@@ -13,9 +13,9 @@ def index(request):
  
 
 def about(request):
-    context_dict = {'message': "This tutorial has been put together by Amy Gillies."}
-    
-    return render(request, 'rango/about.html', context=context_dict)
+    print(request.method)
+    print(request.user)
+    return render(request, 'rango/about.html', {})
 
 def show_category(request, category_name_slug):
 
